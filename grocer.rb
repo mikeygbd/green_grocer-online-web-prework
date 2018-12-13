d2da4a4ea20f3eededc48103662d1c17cbbe3aea
Michael Soares <mikey.soares@gmail.com>
2018-12-13 09:33:49 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,31 @@
+require 'pry'
 def consolidate_cart(cart)
   # code here
+  new_cart = {}
+
+  cart.each do |items|
+    items.each do |item, data|
+    if !new_cart.has_key?(item)
+      new_cart[item] = data
+      new_cart[item][:count] = 1
+    else
+      new_cart[item][:count] += 1
+    end
+    end
+  end
+  new_cart
 end
 
 def apply_coupons(cart, coupons)
   # code here
+  cart.each do |items|
+    items.each do |item, data|
+      if item == coupons[:item]
+        cart[item.join(' ')"W/COUPON"]
+      binding.pry
+      end
+    end
+  end
 end
 
 def apply_clearance(cart)
